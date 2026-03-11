2c4296e740908223824af85ffe7f465809cd371e
3014682779 <3014682779@qq.com>
2026-03-11 21:16:25 +0800

    简单创建，依赖目前只有spring web 和 mysql ,mybetis

---
+++ src/main/java/com/example/chatting_in_web/ChattingInWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatting_in_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingInWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingInWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatting_in_web/ChattingInWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatting_in_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingInWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
