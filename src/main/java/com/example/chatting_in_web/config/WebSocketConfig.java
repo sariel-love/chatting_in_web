70e3f318c165dbfba0f710fc9da8e096fef967ae
3014682779 <3014682779@qq.com>
2026-03-17 21:48:46 +0800

    websocket配置类（session有点问题）

---
+++ src/main/java/com/example/chatting_in_web/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.example.chatting_in_web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Component
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private Chat chat;
+
+    @Autowired
+    private ChatHandShake chatHandShake;
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chat,"/websocket").addInterceptors(chatHandShake);
+    }
+}
