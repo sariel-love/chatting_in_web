a21ff9c244aaa25df7b6668dcb94423bc0ada410
3014682779 <3014682779@qq.com>
2026-03-24 21:45:26 +0800

    处理群聊功能失效问题

--- src/main/java/com/example/chatting_in_web/config/AiChatHandShake.java
+++ src/main/java/com/example/chatting_in_web/config/AiChatHandShake.java
@@ -5,11 +5,13 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import java.util.Map;
 
+@Component
 public class AiChatHandShake implements HandshakeInterceptor {
 
     @Override
