d8f1acffe329391d4eaec5af41f340aa9ffab2a9
3014682779 <3014682779@qq.com>
2026-03-29 16:27:37 +0800

    修改完善User类

--- src/main/java/com/example/chatting_in_web/entity/User.java
+++ src/main/java/com/example/chatting_in_web/entity/User.java
@@ -3,16 +3,20 @@ package com.example.chatting_in_web.entity;
 public class User {
     private Integer id;
     private String username;
-    private String password;
+    public  String sex;
     private String phone_number;
+    private String email;
+    private String password;
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
+                ", sex='" + sex + '\'' +
                 ", phone_number='" + phone_number + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 
@@ -32,12 +36,12 @@ public class User {
         this.username = username;
     }
 
-    public String getPassword() {
-        return password;
+    public String getSex() {
+        return sex;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setSex(String sex) {
+        this.sex = sex;
     }
 
     public String getPhone_number() {
@@ -46,5 +50,21 @@ public class User {
 
     public void setPhone_number(String phone_number) {
         this.phone_number = phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
