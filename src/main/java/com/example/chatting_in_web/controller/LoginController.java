f25bbffc78b7a98255506c8df3fd920a8d4aa0b1
3014682779 <3014682779@qq.com>
2026-04-01 20:45:48 +0800

    增加日志

--- src/main/java/com/example/chatting_in_web/controller/LoginController.java
+++ src/main/java/com/example/chatting_in_web/controller/LoginController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
+@Slf4j
 @RequestMapping("/login")
 public class LoginController {
 
@@ -31,8 +33,8 @@ public class LoginController {
         String result = userService.FindUser_forLogin(loginUser.getPhone_number(),loginUser.getPassword());
         String username = userService.FindUserName(loginUser.getPhone_number());
         loginUser.setUsername(username);
-        System.out.println(result);
         if(result.equals("welcome logining")){//设置session为后续聊天业务做准备
+            log.info("{}登陆成功",username);
             HttpSession session = request.getSession();
             session.setAttribute("loginUser",loginUser);
             if(username != null){
@@ -41,33 +43,35 @@ public class LoginController {
                 cookie.setPath("/");
                 response.addCookie(cookie);
             }else{
-                System.out.println("cookie has an error");
+                log.info("cookie has an error");
             }
+
             return "{\"code\":\"200\",\"message\":\"success\"}";
        }else if(result.equals("Password is not true")){
+            log.info("{}因密码错误无法登录",username);
            return "{\"code\":400\",\"message\":\"false\"}";
         }else{
+            log.info("手机号为{}的用户未注册",loginUser.getPhone_number());
             return "{\"code\":500\",\"message\":\"false\"}";
         }
 
-    }
+        }
     @RequestMapping("/chat")
     public String  toChat(){
+        log.info("跳转到群聊");
         return "chat";
     }
 
     @RequestMapping("/aichat")
     public String  toAiChat(){
+        log.info("跳转到与ai聊天");
         return "aichat";
     }
 
-    @RequestMapping("/testing")
-    public String  toTesting(){
-        return "testing";
-    }
 
     @RequestMapping("/register")
     public String  toRegister(){
+        log.info("跳转到注册页面");
         return "register";
     }
 }
